73b34bced2ee496ef7cabbaea98bbc8229170dd7
Elliot Marsden <elliot.marsden@gmail.com>
2015-10-04 23:24:19 +0100

    Remove unused cross-domain permission

--- serve.py
+++ serve.py
@@ -11,20 +11,17 @@ database = {}
 
 
 @app.route("/")
-# @crossdomain(origin='*')
 def describe():
     return database.get('network').to_json()
 
 
 @app.route("/create/<int:x>,<int:y>")
-# @crossdomain(origin='*')
 def create(x, y):
     database['network'] = ought_ext.Network(x, y)
     return 'Network created successfully'
 
 
 @app.route("/iterate")
-# @crossdomain(origin='*')
 def iterate():
     database['network'].iterate()
     return 'Network iterated successfully'
