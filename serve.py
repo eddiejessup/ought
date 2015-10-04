102d9e2a450d4b015d4139b30130771f70899694
Elliot Marsden <elliot.marsden@gmail.com>
2015-10-04 23:24:48 +0100

    Add basic persistence to server

--- serve.py
+++ serve.py
@@ -1,29 +1,51 @@
 import flask
 import ought_ext
 from flask.ext.cors import CORS
+import redis
 
+
+db = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 app = flask.Flask(__name__)
 app.debug = True
 CORS(app)
 
-database = {}
+state = {}
+
+
+def _get_network():
+    if 'network' in state:
+        return state['network']
+    elif 'network' in db:
+            json = db.get('network')
+            print(type(json))
+            network = ought_ext.Network(json)
+            state['network'] = network
+            return network
+    else:
+        return None
+
+
+def _set_network(network):
+    state['network'] = network
+    db.set('network', network.to_json())
 
 
 @app.route("/")
 def describe():
-    return database.get('network').to_json()
+    return _get_network().to_json()
 
 
 @app.route("/create/<int:x>,<int:y>")
 def create(x, y):
-    database['network'] = ought_ext.Network(x, y)
+    network = ought_ext.Network(x, y)
+    _set_network(network)
     return 'Network created successfully'
 
 
 @app.route("/iterate")
 def iterate():
-    database['network'].iterate()
+    _get_network().iterate()
     return 'Network iterated successfully'
 
 if __name__ == "__main__":
